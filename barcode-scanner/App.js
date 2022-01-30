1da859bf4d1d11a23496915c50a5f5ac9fa1df67
Jonathan Stroz <jonathanstroz@Jonathans-Air.hitronhub.home>
2022-01-30 16:30:12 -0400

    UI Init

--- barcode-scanner/App.js
+++ barcode-scanner/App.js
@@ -1,10 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import Homepage from './ui/Homepage.js'
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Homepage/>
       <StatusBar style="auto" />
     </View>
   );
---
+++ barcode-scanner/ui/Homepage.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { FlatList, StyleSheet, Text, View, Image, ListView, Button} from 'react-native';
+
+
+export default class Homepage extends React.Component {
+  constructor() {
+    super(); 
+    this.state = {}
+  }
+
+  render() {
+    return (
+        <View>
+          <Text>Test to see if this works</Text> 
+        </View>
+    );
+  }
+}
+
+
+const styles = StyleSheet.create({});
